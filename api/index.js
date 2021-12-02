f03b15210320217142625545206bdc530679ed34
SK MIRAJ <62544210+miraj0507@users.noreply.github.com>
2021-12-02 22:03:18 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
-
+//
 app.listen("5000", () => {
   console.log("Backend is running.");
 });
